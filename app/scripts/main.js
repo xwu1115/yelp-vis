4a4a38278b3f5c47d44e592bd5a3653c194d229f
xwu1115 <xwu1115@gmail.com>
2015-11-14 16:09:09 -0500

    setup

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global YelpInfoVis, $*/
+
+
+window.YelpInfoVis = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    YelpInfoVis.init();
+});
